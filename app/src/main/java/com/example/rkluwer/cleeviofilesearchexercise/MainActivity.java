409e64d439dd7a4cc32ca9af5245e24422f5241a
Robbert Kluwer <robbertkluwer@hotmail.com>
2018-07-17 22:09:42 +0200

    listView creation

--- app/src/main/java/com/example/rkluwer/cleeviofilesearchexercise/MainActivity.java
+++ app/src/main/java/com/example/rkluwer/cleeviofilesearchexercise/MainActivity.java
@@ -12,9 +12,30 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
+
+    private String[] filePathStrings;
+    private File[] fileArray;
+    private File file;
+
+    private int count = 0;
+    private ArrayList<String> pathHistory;
+    private String lastDirectory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +44,49 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Setting the "return" icon in the toolbar
+        // The return icon makes the user go back one step in the file menu.
+        toolbar.setNavigationIcon(getDrawable(R.mipmap.icon_left48));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (count != 0){
+                    pathHistory.remove(count);
+                    count--;
+                    loadInternalStorage();
+                }
+            }
+        });
+
+        listView = findViewById(R.id.main_layout_list_view);
+
+        // Initial check on the permissions and clearing the path history.
         if (isExternalStorageReadable()){
             Toast.makeText(this, "External storage readable", Toast.LENGTH_SHORT).show();
             requestPermission();
+
+            clearPathHistory();
         }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String currentDirectory = pathHistory.get(count) + "/" + (String) parent.getItemAtPosition(position);
+                createLog(currentDirectory);
+                File file = new File(currentDirectory);
+
+                if (file.isDirectory()) {
+                    count++;
+                    pathHistory.add(count, currentDirectory);
+                    loadInternalStorage();
+                } else if (file.isAbsolute()){
+                    createLog("This is a file that needs to be opened with an intent");
+                    // TODO open the file with an intent
+                } else {
+                    createLog("Something has gone wrong");
+                }
+            }
+        });
     }
 
     @Override
@@ -51,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
                 createLog("Setting menu item clicked");
                 return true;
             case R.id.action_refresh:
+                clearPathHistory();
                 loadInternalStorage();
                 createLog("Refresh menu item clicked");
                 return true;
@@ -60,7 +121,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadInternalStorage() {
-        // TODO load the internal storage
+        try {
+            file = new File(pathHistory.get(count));
+            createLog("FilePathHistory: " + pathHistory.get(count));
+            fileArray = file.listFiles();
+
+            filePathStrings = new String[fileArray.length];
+            for (int i = 0; i < fileArray.length; i++) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(fileArray[i].toString());
+                builder.delete(0, pathHistory.get(count).length() + 1);
+                filePathStrings[i] = builder.toString();
+                createLog("File directory is: " + filePathStrings[i]);
+            }
+
+            arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, filePathStrings);
+            listView.setAdapter(arrayAdapter);
+        } catch (NullPointerException e){
+            createLog(e.getMessage());
+        }
+    }
+
+    // This method clears the path history and starts the list of files again from the beginning.
+    private void clearPathHistory() {
+        count = 0;
+        pathHistory = new ArrayList<>();
+        pathHistory.add(System.getenv("EXTERNAL_STORAGE"));
     }
 
     // This method checks whether the internal storage is readable.
